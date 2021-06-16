b492d2cff48352eb05f1d1952fa1c3c48b4410e7
ChenThomas0607 <109021037@live.asia.edu.tw>
2021-06-16 17:50:39 +0800

    f5

--- demo.py
+++ demo.py
@@ -21,58 +21,56 @@ def day(y,m):
     url = "https://168.motc.gov.tw/countrydeadhurt/%E8%87%BA%E4%B8%AD%E5%B8%82?"+"y="+str(y)+"&m="+str(m)
     return url
     
-def people(url):
-    data=requests.get(url)
-    soup = BeautifulSoup(data.text, 'lxml')
+def dead(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'lxml')
+    result0 = soup.find_all('script')
+    data = result0[2].string.split('''"''')
+    return data[3]
+
+def hurt(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'lxml')
     result0 = soup.find_all('script')
     data = result0[2].string
-    data = data.replace('''"PR":''',"")
-    data = data.replace('''"''',"")
-    data = data.replace('''}''',"")
-    data = data.split(",")
-    return data[38]
+    data = data.replace(",","")
+    data = data.split('''"''')
+    return data[7]
 
-month = []
-value = []
+deadlist = []
+hurtlist = []
 
 def answer(year):
     for i in range(1,13):
-        month.append(int(i))
-        value.append(float(people(day(year,i))))
-        print(str(year)+"年"+str(i)+"月"+"-->"+people(day(year,i)))
-    plt.plot(month, value)
-    plt.show()
+        deadlist.append(int(dead(day(year,i))))
+        hurtlist.append(int(hurt(day(year,i))))
+        print(str(year)+"年"+str(i)+"月"+"-->"+"死亡人數:"+str(deadlist[i-1])+","+"受傷人數:"+str(hurtlist[i-1]))
 
 
 
-btn1 = Button(window, text="100", command=lambda:answer("100"))
+btn1 = Button(window, text="100", bg="#4DFFFF", font=("Helvetica 25 bold"), command=lambda:answer("100"))
 btn1.grid(row=0, column=0, sticky=NSEW)
-btn2 = Button(window, text="101", command=lambda:answer("101"))
+btn2 = Button(window, text="101", bg="#FFFF37", font=("Helvetica 25 bold"), command=lambda:answer("101"))
 btn2.grid(row=1, column=0, sticky=NSEW)
-btn3 = Button(window, text="102", command=lambda:answer("102"))
+btn3 = Button(window, text="102", bg="#4DFFFF", font=("Helvetica 25 bold"), command=lambda:answer("102"))
 btn3.grid(row=2, column=0, sticky=NSEW)
-btn4 = Button(window, text="103", command=lambda:answer("103"))
+btn4 = Button(window, text="103", bg="#FFFF37", font=("Helvetica 25 bold"), command=lambda:answer("103"))
 btn4.grid(row=3, column=0, sticky=NSEW)
-btn5 = Button(window, text="104", command=lambda:answer("104"))
+btn5 = Button(window, text="104", bg="#4DFFFF", font=("Helvetica 25 bold"), command=lambda:answer("104"))
 btn5.grid(row=4, column=0, sticky=NSEW)
 
-btn6 = Button(window, text="105", command=lambda:answer("105"))
+btn6 = Button(window, text="105", bg="#FFFF37", font=("Helvetica 25 bold"), command=lambda:answer("105"))
 btn6.grid(row=0, column=1, sticky=NSEW)
-btn7 = Button(window, text="106", command=lambda:answer("106"))
+btn7 = Button(window, text="106", bg="#4DFFFF", font=("Helvetica 25 bold"), command=lambda:answer("106"))
 btn7.grid(row=1, column=1, sticky=NSEW)
-btn8 = Button(window, text="107", command=lambda:answer("107")) 
+btn8 = Button(window, text="107", bg="#FFFF37", font=("Helvetica 25 bold"), command=lambda:answer("107")) 
 btn8.grid(row=2, column=1, sticky=NSEW)
-btn9 = Button(window, text="108", command=lambda:answer("108"))
+btn9 = Button(window, text="108", bg="#4DFFFF", font=("Helvetica 25 bold"), command=lambda:answer("108"))
 btn9.grid(row=3, column=1, sticky=NSEW)
-btn10 = Button(window, text="109", command=lambda:answer("109"))
+btn10 = Button(window, text="109", bg="#FFFF37", font=("Helvetica 25 bold"), command=lambda:answer("109"))
 btn10.grid(row=4, column=1, sticky=NSEW)
 
-btn10 = Button(window, text="進入題目", command=lambda:answer("109")) 
-btn10.grid(row=5, column=0, columnspan=2, sticky=NSEW)
-
-
-
-
-
+btnExit = Button(window, text="Exit", bg="#FF8040", font=("Helvetica 25 bold"), command=window.destroy)
+btnExit.grid(row=5, column=0, columnspan=2, sticky=NSEW)
 
 window.mainloop()
